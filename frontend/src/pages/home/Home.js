2fe55252d5580a545c28533e2a6fd140b25b8384
basil george <baezl4life@gmail.com>
2022-01-28 02:04:10 +0200

    fetching data from backend and adding pagination

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ReactPaginate from "react-paginate";
 import "./Home.scss";
 
 const Home = () => {
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(null);
+  const [itemCount, setItemCount] = useState(null);
+  const [filter, setFilter] = useState({
+    sensorType: "",
+    location: "",
+  });
+
+  useEffect(() => {
+    const getFarms = async () => {
+      const { data } = await axios.get(
+        `http://localhost:8080/api/farms?${
+          filter.location !== "" ? `location=${filter.location}` : " "
+        }${
+          filter.sensorType !== "" ? `&sensorType=${filter.sensorType}` : ""
+        }&page=${page}`
+      );
+      setItems(data.data);
+      setItemCount(data.totalFarms);
+      setTotalPage(data.pages);
+      console.log(data);
+    };
+    getFarms();
+  }, [page, filter.location, filter.sensorType]);
+
+  const handlePageClick = (data) => {
+    setPage(data.selected + 1);
+  };
   return (
     <div className="home">
       <div className="filter">
         <label>
           Location:{" "}
-          <select>
+          <select
+            onChange={(e) => setFilter({ ...filter, location: e.target.value })}
+          >
             <option value="">all</option>
             <option value="Friman Metsola collective">
               Friman Metsola collective
@@ -23,7 +56,11 @@ const Home = () => {
         </label>
         <label>
           sensorType:{" "}
-          <select>
+          <select
+            onChange={(e) =>
+              setFilter({ ...filter, sensorType: e.target.value })
+            }
+          >
             <option value="">all</option>
             <option value="pH">pH</option>
             <option value="rainFall">rainFall</option>
@@ -42,44 +79,34 @@ const Home = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>England</td>
-            <td>24 jun 2019</td>
-            <td>Temperature</td>
-            <td>2</td>
-          </tr>
-          <tr>
-            <td>England</td>
-            <td>24 jun 2019</td>
-            <td>Temperature</td>
-            <td>2</td>
-          </tr>
-          <tr>
-            <td>England</td>
-            <td>24 jun 2019</td>
-            <td>Temperature</td>
-            <td>2</td>
-          </tr>
-          <tr>
-            <td>England</td>
-            <td>24 jun 2019</td>
-            <td>Temperature</td>
-            <td>2</td>
-          </tr>
-          <tr>
-            <td>England</td>
-            <td>24 jun 2019</td>
-            <td>Temperature</td>
-            <td>2</td>
-          </tr>
-          <tr>
-            <td>England</td>
-            <td>24 jun 2019</td>
-            <td>Temperature</td>
-            <td>2</td>
-          </tr>
+          {items.map((i) => (
+            <tr key={i._id}>
+              <td>{i.location}</td>
+              <td>{new Date(i.datetime).toDateString()}</td>
+              <td>{i.sensorType}</td>
+              <td>{i.value}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
+
+      <ReactPaginate
+        previousLabel={"previous"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        pageCount={totalPage}
+        marginPagesDisplayed={6}
+        pageRangeDisplayed={3}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        pageClassName={"pagination__item"}
+        pageLinkClassName={"pagination-link"}
+        previousLinkClassName={"pagination__item"}
+        nextLinkClassName={"pagination__item"}
+        breakClassName={"pagination__item"}
+        breakLinkClassName={"pagination__item"}
+        activeClassName={"pagination__item active"}
+      />
     </div>
   );
 };
